7d9002f195a7f473fb974c777c9d64a58f34d7de
h01nait <h01nait@gmail.com>
2023-04-25 13:54:07 -0700

    put.c

--- _put.c
+++ _put.c
@@ -14,7 +14,6 @@ int _puts(char *str)
 		_putchar(*str++);
 	return (str - a);
 }
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -36,4 +35,5 @@ int _putchar(int c)
 		buf[i++] = c;
 	return (1);
 }
+
 
