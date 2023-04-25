909955579415461a55c45b4829d3374efd82a1ae
h01nait <h01nait@gmail.com>
2023-04-25 13:57:32 -0700

    P.function

--- print_functions.c
+++ print_functions.c
@@ -7,6 +7,7 @@
  *
  * Return: number chars printed
  */
+
 int print_char(va_list ap, params_t *params)
 {
 	char pad_char = ' ';
@@ -28,6 +29,7 @@ int print_char(va_list ap, params_t *params)
  *
  * Return: number chars printed
  */
+
 int print_int(va_list ap, params_t *params)
 {
 	long l;
@@ -48,6 +50,7 @@ int print_int(va_list ap, params_t *params)
  *
  * Return: number chars printed
  */
+
 int print_string(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *), pad_char = ' ';
@@ -55,32 +58,32 @@ int print_string(va_list ap, params_t *params)
 
 	(void)params;
 	switch ((int)(!str))
-		case 1:
-			str = NULL_STRING;
+	case 1:
+		str = NULL_STRING;
 
-	j = pad = _strlen(str);
-	if (params->precision < pad)
-		j = pad = params->precision;
+		j = pad = _strlen(str);
+		if (params->precision < pad)
+			j = pad = params->precision;
 
-	if (params->minus_flag)
-	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
-	}
-	while (j++ < params->width)
-		sum += _putchar(pad_char);
-	if (!params->minus_flag)
-	{
-		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
-				sum += _putchar(*str++);
-		else
-			sum += _puts(str);
-	}
-	return (sum);
+		if (params->minus_flag)
+		{
+			if (params->precision != UINT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum += _puts(str);
+		}
+		while (j++ < params->width)
+			sum += _putchar(pad_char);
+		if (!params->minus_flag)
+		{
+			if (params->precision != UINT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum += _puts(str);
+		}
+		return (sum);
 }
 
 /**
@@ -90,6 +93,7 @@ int print_string(va_list ap, params_t *params)
  *
  * Return: number chars printed
  */
+
 int print_percent(va_list ap, params_t *params)
 {
 	(void)ap;
@@ -104,6 +108,7 @@ int print_percent(va_list ap, params_t *params)
  *
  * Return: number chars printed
  */
+
 int print_S(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *);
